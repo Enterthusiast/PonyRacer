9f82468c55e6429ee0c472267b6f739b39bb4317
dbernard <dbernard@leadersleague.com>
2016-11-08 18:55:37 +0100

    change page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'ponyracer-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  viewProviders: [Title]
 })
 export class PonyRacerAppComponent {
+
+  constructor(title: Title) {
+    title.setTitle('PonyRacer - Bet on ponies');
+  }
+
   title = 'PonyRacer';
+
   numberOfUsers: number = 146;
+
   user: any = { name: 'Cédric' };
+
   onNewRace(event) {
     console.log(event);
     event.preventDefault();
     event.stopPropagation();
     return 'A new race just begun!'
   }
+
 }
